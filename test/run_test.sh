cb13aa64675fa35442a55079cd4ce78105378a69
Devashish Tripathi <dxt563@student.bham.ac.uk>
2026-09-21 14:10:55 +0000

    add sensitivity usage in NoisyActivation, debug locs variable, add sensitivity calc file

--- test/run_test.sh
+++ test/run_test.sh
@@ -1,12 +1,40 @@
+CKPT_PATH="/home/devashish_tripathi/projects/sanity_bdct/ckpt"
+DATA_ROOT="/home/devashish_tripathi/projects/sanity_data/data_bdct"
+BIN_NAME="val_dummy.bin"
+MODEL_NAME="sample"
+DATA_NAME="dummy"
+BATCH_SIZE=2
+RANDOM_SEED=1337
+EPSILON=4
+EPOCH=-1
+
+# TOGGLES
+USE_NO_NOISE=false # keep false to mimic baseline code
+USE_NO_LOCS=false # keep false to mimic baseline code
+USE_SENSITIVITY=false # keep false to mimic baseline code
+SENS_PTH="/home/devashish_tripathi/projects/sanity_data/sensitivity.pt"
+
+if [ "$USE_NO_NOISE" = true ]; then
+    EXTRA_ARGS+=(--no_noise)
+fi
+
+if [ "$USE_NO_LOCS" = true ]; then
+    EXTRA_ARGS+=(--no_locs)
+fi
+
+if [ "$USE_SENSITIVITY" = true ]; then
+    EXTRA_ARGS+=(--use_sensitivity --sens_pth "$SENS_PTH")
+fi
+
 python test_dctdp.py \
---ckpt_path /home/devashish_tripathi/projects/sanity_bdct/ckpt \
---data_root /home/devashish_tripathi/projects/sanity_data/data_bdct \
---bin_name val_dummy.bin \
---model_name sample \
---data_name dummy \
---batch_size 2 \
---random_seed 1337 \
---epsilon 4 \
+--ckpt_path $CKPT_PATH \
+--data_root $DATA_ROOT \
+--bin_name $BIN_NAME \
+--model_name $MODEL_NAME \
+--data_name $DATA_NAME \
+--batch_size $BATCH_SIZE \
+--random_seed $RANDOM_SEED \
+--epsilon $EPSILON \
 --gpu_ids 0 \
---use_noise \
---epoch -1
+--epoch $EPOCH \
+"${EXTRA_ARGS[@]}"
